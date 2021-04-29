6ca09f1380e71ec91cad1dcec1c7a7a0235bc369
Vadim Stishanok <stishanok@gmail.com>
2021-04-29 05:08:55 +0300

    Add project files.

---
+++ Window.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace DataAdapter
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.buttonFill = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.Location = new System.Drawing.Point(12, 12);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(594, 74);
+            this.textBox.TabIndex = 0;
+            this.textBox.Text = "WAITFOR DELAY \'00:00:02\'; SELECT * FROM Products;";
+            // 
+            // buttonFill
+            // 
+            this.buttonFill.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonFill.Location = new System.Drawing.Point(12, 92);
+            this.buttonFill.Name = "buttonFill";
+            this.buttonFill.Size = new System.Drawing.Size(132, 34);
+            this.buttonFill.TabIndex = 1;
+            this.buttonFill.Text = "Fill";
+            this.buttonFill.UseVisualStyleBackColor = true;
+            this.buttonFill.Click += new System.EventHandler(this.ClickFill);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUpdate.Location = new System.Drawing.Point(150, 92);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(132, 34);
+            this.buttonUpdate.TabIndex = 2;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.ClickUpdate);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 132);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(594, 327);
+            this.dataGridView.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(288, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 34);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Async";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ClickAsync);
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 471);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonFill);
+            this.Controls.Add(this.textBox);
+            this.Name = "Window";
+            this.Text = "DataAdapter MS SQL Server";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button buttonFill;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Window.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace DataAdapter
+{
+    public partial class Window : Form
+    {
+        private SqlConnection connection = null;
+        private SqlDataAdapter adapter = null;
+        private SqlCommandBuilder commandBuilder = null;
+        private DataSet dataSet = null;
+        private DataTable table = null;
+        private string connectionString = "";
+
+        public Window()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+        }
+
+        private void ClickFill(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                string commandSelectText = textBox.Text;
+                adapter = new SqlDataAdapter(commandSelectText, connection);
+                commandBuilder = new SqlCommandBuilder(adapter);
+                dataSet = new DataSet();
+                adapter.Fill(dataSet, "Products");
+                dataGridView.DataSource = dataSet.Tables["Products"];
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        private void ClickUpdate(object sender, EventArgs e)
+        {
+            adapter.Update(dataSet, "Products");
+        }
+
+        private void ClickAsync(object sender, EventArgs e)
+        {
+            const string asyncEnable = "Asynchronous Processing=true";
+
+            if (!connectionString.Contains(asyncEnable))
+            {
+                connectionString = String.Format("{0}; {1}", connectionString, asyncEnable);
+            }
+
+            connection = new SqlConnection(connectionString);
+            SqlCommand command = connection.CreateCommand();
+
+            command.CommandText = "WAITFOR DELAY '00:00:02'; SELECT * FROM Products;";
+            command.CommandType = CommandType.Text;
+            command.CommandTimeout = 30;
+
+            try
+            {
+                connection.Open();
+                AsyncCallback callback = new AsyncCallback(GetDataCallback);
+                command.BeginExecuteReader(callback, command);
+                MessageBox.Show("Added thread is working...");
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void GetDataCallback(IAsyncResult result)
+        {
+            SqlDataReader reader = null;
+            try
+            {
+                SqlCommand command = (SqlCommand) result.AsyncState;
+                reader = command.EndExecuteReader(result);
+                table = new DataTable();
+                int line = 0;
+
+                do
+                {
+                    while (reader.Read())
+                    {
+                        if (line == 0)
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                table.Columns.Add(reader.GetName(i));
+                            }
+                        }
+
+                        line++;
+                        DataRow row = table.NewRow();
+
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            row[i] = reader[i];
+                        }
+
+                        table.Rows.Add(row);
+                    }
+
+                } while (reader.NextResult());
+
+                DgvAction();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("From Callback 1:" + ex.Message);
+            }
+            finally
+            {
+                try
+                {
+                    if (!reader.IsClosed)
+                    {
+                        reader.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("From Callback 2:" + ex.Message);
+                }
+            }
+        }
+
+        private void DgvAction()
+        {
+            if (dataGridView.InvokeRequired)
+            {
+                dataGridView.Invoke(new Action(DgvAction));
+                return;
+            }
+
+            dataGridView.DataSource = table;
+        }
+    }
+}
